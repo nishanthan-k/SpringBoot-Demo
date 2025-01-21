3aba66bd994c70f5552bd49043f0a85055bf115e
nishanthan-k <krn.nishanthan@gmail.com>
2025-01-21 23:25:54 +0530

    feat: product model

---
+++ src/main/java/com/nishanthan/ecom/EcomApplication.java
@@ -0,0 +1,15 @@
+package com.nishanthan.ecom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan
+public class EcomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcomApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/nishanthan/ecom/model/Product.java
@@ -0,0 +1,29 @@
+package com.nishanthan.ecom.model;
+
+import java.math.BigDecimal;
+import java.sql.Date;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Product {
+
+  @Id
+  private int id;
+  private String name;
+  private String desc;
+  private String brand;
+  private BigDecimal price;
+  private String category;
+  private Date releaseDate;
+  private boolean available;
+  private int quantity;
+
+}
